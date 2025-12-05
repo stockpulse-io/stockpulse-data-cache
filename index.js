1eb07de3526c24e5cab5b8eb9a7300fb53081c82
Anmol Jamba <anmoljamba18@gmail.com>
2025-12-05 09:00:40 +0530

    feat: initialize Kafka consumer in index.js for app startup

---
+++ index.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const { handleError } = require("./src/helpers/errorHandler");
+const runConsumer = require("./src/kafka/consumer");
+
+(async () => {
+  try {
+    console.log("Starting StockPulse Data Cache Service...");
+    await runConsumer();
+  } catch (err) {
+    handleError(err, "FAILED_TO_START_SERVICE");
+    process.exit(1);
+  }
+})();
